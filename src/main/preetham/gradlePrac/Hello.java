e6f744ac656c594feb5bb66a17a99c546c093d64
preethamsathyamurthy <preethamsathyamurthy@gmail.com>
2018-08-30 15:43:46 +0530

    javaProject

---
+++ src/main/preetham/gradlePrac/Hello.java
@@ -0,0 +1,15 @@
+package preetham.gradlePrac;
+
+import java.util.Arrays;
+
+import org.apache.commons.lang3.StringUtils;
+
+
+public class Hello{
+
+	public static void main(String args[])
+	{
+		System.out.println(StringUtils.swapCase(Arrays.toString(args)));
+	}
+
+}
